fe37ec0d46bc5be93559ece626496a87c9b5f8f5
LawX22 <crmchs.carisusa.lawrenzxavier@gmail.com>
2024-05-30 18:23:53 +0800

    New Addition

--- Entities/CarisusaUtangananContext.cs
+++ Entities/CarisusaUtangananContext.cs
@@ -20,7 +20,6 @@ public partial class CarisusaUtangananContext : DbContext
     public virtual DbSet<UserType> UserTypes { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CarisusaUtanganan;TrustServerCertificate=true;Trusted_Connection=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
