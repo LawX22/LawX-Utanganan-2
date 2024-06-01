a60f31839d14f05416eddf757d28df45247f8495
LawX22 <crmchs.carisusa.lawrenzxavier@gmail.com>
2024-06-01 17:05:35 +0800

    New Addtition

---
+++ Entites/CarisusaUtangananContext.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LawX_Utanganan.Entites;
+
+public partial class CarisusaUtangananContext : DbContext
+{
+    public CarisusaUtangananContext()
+    {
+    }
+
+    public CarisusaUtangananContext(DbContextOptions<CarisusaUtangananContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ClientInfo> ClientInfos { get; set; }
+
+    public virtual DbSet<Loan> Loans { get; set; }
+
+    public virtual DbSet<UserType> UserTypes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=CarisusaUtanganan;TrustServerCertificate=true;Trusted_Connection=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ClientInfo>(entity =>
+        {
+            entity.ToTable("ClientInfo");
+
+            entity.Property(e => e.Address)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Birthdate).HasColumnType("date");
+            entity.Property(e => e.CivilStatus)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("Civil_Status");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.MiddleName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.NameOfFather)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.NameOfMother)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Occupation)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Religion)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Loan>(entity =>
+        {
+            entity.ToTable("Loan");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Amount).HasColumnName("amount");
+            entity.Property(e => e.Borrower).HasColumnName("borrower");
+            entity.Property(e => e.DateCreated)
+                .HasColumnType("datetime")
+                .HasColumnName("dateCreated");
+            entity.Property(e => e.Deduction).HasColumnName("deduction");
+            entity.Property(e => e.DueDate)
+                .HasColumnType("datetime")
+                .HasColumnName("dueDate");
+            entity.Property(e => e.Interest).HasColumnName("interest");
+            entity.Property(e => e.InterestAmount).HasColumnName("interestAmount");
+            entity.Property(e => e.Payment).HasColumnName("payment");
+            entity.Property(e => e.PaymentAmount).HasColumnName("paymentAmount");
+            entity.Property(e => e.RecievableAmount).HasColumnName("recievableAmount");
+            entity.Property(e => e.Status)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("status");
+            entity.Property(e => e.Term).HasColumnName("term");
+            entity.Property(e => e.TotalAmount).HasColumnName("totalAmount");
+        });
+
+        modelBuilder.Entity<UserType>(entity =>
+        {
+            entity.ToTable("UserType");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Entites/UserType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LawX_Utanganan.Entites;
+
+public partial class UserType
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+}
